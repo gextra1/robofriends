ddaa4bab7e577c1ceecd1db023bffde745b590a5
gextra1 <georgyextra@gmail.com>
2021-03-04 14:58:13 -0500

    Updates

---
+++ precache-manifest.341f01a64293934ec816517f9baa9621.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca8401db4a7e7788d2230146966a388",
+    "url": "/robofriens/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriens/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "35ace370256cdddf6ba9",
+    "url": "/robofriens/static/css/main.a158c486.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriens/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriens/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35ace370256cdddf6ba9",
+    "url": "/robofriens/static/js/main.449d01e9.chunk.js"
+  },
+  {
+    "revision": "79f75794ff34820d0444",
+    "url": "/robofriens/static/js/runtime-main.a40b3789.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriens/static/media/SEGA.12a3db8b.woff"
+  }
+]);
